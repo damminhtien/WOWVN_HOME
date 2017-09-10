a288ad77ebc781ce3bc84a803de49556dbabcb4c
damminhtien <damminhtienchl@gmail.com>
2017-09-10 20:37:18 +0700

    up heroku3

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.use(express.static("./public"));
 app.set("view engine","ejs");
 app.set("views","./views");
 
+var server = require("http").Server(app);
+server.listen(process.env.PORT || 3000);
+
 app.listen(process.env.PORT || 2211, function(){
     console.log("Server connected");
 });
